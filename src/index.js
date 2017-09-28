98cbfff32f7db1b0ce275b71567eef2b19d7c261
Darya Milko <milkodaria@gmail.com>
2017-09-28 23:13:56 +0300

    done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    if (str.length %2!= 0){
+      return false;
+    }
+    var arraySplit = str.split('');
+    var arrayInput = [];
+    for (var i=0; i<arraySplit.length; i++){
+      for(var j=0; j<bracketsConfig.length; j++) {
+        if(arrayInput[arrayInput.length-1] == bracketsConfig[j][0] && arraySplit[i] == bracketsConfig[j][1]){
+          arrayInput.pop();
+        } else {
+          if(arraySplit[i] == bracketsConfig[j][0]) {
+            arrayInput.push(arraySplit[i]);
+          }
+        }
+      }
+    }
+    return arrayInput.length == 0;
 }
